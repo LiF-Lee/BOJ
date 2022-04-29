cfcdb5d22dfe767dfaacfc558d6e43a642e275e7
Lee SJ <ei@genshin.ai>
2022-04-29 22:40:22 +0900

    no message

---
+++ src/python/1107.py
@@ -0,0 +1,17 @@
+from sys import stdin, stdout
+target = int(stdin.readline())
+btn_count = int(stdin.readline())
+press_count = abs(target - 100)
+if btn_count == 0:
+    stdout.write(f"{len(str(target))}")
+    exit()
+btn_list = list(map(str, stdin.readline().split()))
+if btn_count == 10 or target == 100:
+    stdout.write(f"{press_count}")
+    exit()
+min_val, max_val = 0, 1000001
+for i in range(min_val, max_val):
+    if any(ext in str(i) for ext in btn_list):
+        continue
+    press_count = min(press_count, len(str(i)) + abs(target - i))
+stdout.write(str(press_count))
