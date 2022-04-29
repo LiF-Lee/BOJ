fc56a37e056773dcdc561594e0f4314f7a9a0370
Lee SJ <ei@genshin.ai>
2022-04-29 09:28:58 +0900

    no message

--- src/python/10773.py
+++ src/python/10773.py
@@ -1,20 +1,14 @@
 from sys import stdin, stdout
 n = int(stdin.readline())
-data = [int(stdin.readline()) for _ in range(n)]
-index_n = 0
+data = [int(stdin.readline()) for _ in range(n)][::-1]
+r_sum = 0
+d_count = 0
 for i in range(n):
-    print('-', index_n)
     if data[i] == 0:
-        data[index_n] = 0
-        index_n -= 1
-        if (data[index_n] == 0):
-            for j in range(index_n):
-                if data[index_n - j] != 0:
-                    index_n -= j
-                    break
+        d_count += 1
         continue
-    index_n = i
-s_result = 0
-for k in range(n):
-    s_result += data[k]
-stdout.write("%s" % s_result)
+    if d_count > 0:
+        d_count -= 1
+        continue
+    r_sum += data[i]
+stdout.write("%d" % r_sum)
