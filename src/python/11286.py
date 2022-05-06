4f52ac3faf5bc954d78e41c8cfaaf1a4869bd94f
Lee SJ <ei@genshin.ai>
2022-05-06 20:19:58 +0900

    # heapq

--- src/python/11286.py
+++ src/python/11286.py
@@ -1,20 +1,13 @@
-from ast import Lambda
 from sys import stdin, stdout
-N = int(stdin.readline())
-L = []
-for _ in range(N):
-    R = int(stdin.readline())
-    if R != 0:
-        L.append(R)
-        continue
-    if len(L) != 0:
-        L.sort(key=lambda x: abs(x))
-        txt = L[0]
-        if txt > 0:
-            txt = '-' + str(txt)
-        else:
-            txt = str(abs(txt))
-        stdout.write(f"{txt}\n")
-        L.remove(L[0])
+import heapq
+L, R = [], []
+for i in range(int(stdin.readline())):
+    s = int(stdin.readline().rstrip())
+    if s == 0:
+        if len(L) == 0:
+            R.append(0)
+            continue
+        R.append(heapq.heappop(L)[1])
     else:
-        stdout.write(f"{0}\n")
+        heapq.heappush(L, (abs(s), s))
+print(*R, sep='\n')
