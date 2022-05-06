55732b84e9ce2101d38276c8b65933a45aa9a251
Lee SJ <ei@genshin.ai>
2022-05-06 20:20:20 +0900

    no message

--- src/python/7662.py
+++ src/python/7662.py
@@ -1,40 +1,27 @@
-from sys import stdin, stdout
+import sys;read=sys.stdin.readline
 import heapq
 
-def heap_sort(nums):
-  heap = []
-  for num in nums:
-    heapq.heappush(heap, num)
-  sorted_nums = []
-  while heap:
-    sorted_nums.append(heapq.heappop(heap))
-  return sorted_nums
-
-for _ in range(int(stdin.readline())):
-    L = []
-    a = int(stdin.readline())
-    con = True
-    is_sort = False
-    for i in range(a):
-        cmd = stdin.readline().rstrip().split()
-        if cmd[0] == 'I':
-            is_sort = False
-            heapq.heappush(L, int(cmd[1]))
+result=[]
+for T in range(int(read())):
+    visited=[False]*1_000_001
+    minH,maxH=[],[]
+    for i in range(int(read())):
+        s=read().split()
+        if s[0]=='I':
+            heapq.heappush(minH,(int(s[1]),i))
+            heapq.heappush(maxH,(-int(s[1]),i))
+            visited[i]=True
+        elif s[1]=='1':
+            while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+            if maxH:
+                visited[maxH[0][1]]=False
+                heapq.heappop(maxH)
         else:
-            if con == False:
-                continue
-            if len(L) == 0:
-                con = False
-                continue
-            if cmd[1] == '-1':
-                heapq.heappop(L)
-            else:
-                if is_sort == False:
-                    is_sort = True
-                    L = heap_sort(L)
-                L.pop()
-    if con == False:
-        stdout.write('EMPTY\n')
-    else:
-        L = heap_sort(L)
-        print(L[-1], L[0])
+            while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+            if minH:
+                visited[minH[0][1]]=False
+                heapq.heappop(minH)
+    while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+    while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+    result.append(f'{-maxH[0][0]} {minH[0][0]}'if maxH and minH else'EMPTY')
+print('\n'.join(result))
