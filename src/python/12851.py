d8f97670f0d059468df6744b641ec8920bc2dcfa
Lee SJ <ei@genshin.ai>
2022-05-15 23:34:15 +0900

    no message

--- src/python/12851.py
+++ src/python/12851.py
@@ -4,22 +4,25 @@ from collections import deque
 N, K = map(int, stdin.readline().split())
 
 MAX = 100_000
-L = deque([[-1, 0] for _ in range(MAX + 1)])
+L = deque([-1 for _ in range(MAX + 1)])
+M = deque([-1 for _ in range(MAX + 1)])
 Q = deque([N])
 
-L[N][0] = 0
-L[N][1] = 1
+L[N] = 0
 
 while Q:
     x = Q.popleft()
+    if x == K:
+        stdout.write(f"{L[x]}\n")
+        c = x
+        r = []
+        for _ in range(L[x] + 1):
+            r.append(str(c))
+            c = M[c]
+        stdout.write(f"{' '.join(r[::-1])}")
+        break
     for i in [x - 1, x + 1, x * 2]:
-        if 0 <= i <= MAX:
-            if L[i][0] == -1:
-                L[i][0] = L[x][0] + 1
-                L[i][1] += L[x][1]
-                Q.append(i)
-            elif L[i][0] == L[x][0] + 1:
-                L[i][1] += L[x][1]
-
-print(L[K][0])
-print(L[K][1])
+        if 0 <= i <= MAX and L[i] == -1:
+            M[i] = x
+            Q.append(i)
+            L[i] = L[x] + 1
