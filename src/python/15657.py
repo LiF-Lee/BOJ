6487a206e659970538e557c838592cc260cf2b86
DESKTOP-B6LP876\Hie_sang <ei@genshin.ai>
2022-05-17 21:51:16 +0900

    no message

---
+++ src/python/15657.py
@@ -0,0 +1,8 @@
+from sys import stdin, stdout
+from itertools import combinations_with_replacement
+M, N = map(int, stdin.readline().split())
+L = list(map(str, stdin.readline().split()))
+L.sort()
+
+for i in combinations_with_replacement(L, N):
+    print(' '.join(map(str, i)))
