bf6c32971d0cf93fcf639fff778fdb21cb1e7896
DESKTOP-B6LP876\Hie_sang <ei@genshin.ai>
2022-05-17 22:32:12 +0900

    no message

--- src/python/15657.py
+++ src/python/15657.py
@@ -1,7 +1,7 @@
 from sys import stdin, stdout
 from itertools import combinations_with_replacement
 M, N = map(int, stdin.readline().split())
-L = list(map(str, stdin.readline().split()))
+L = list(map(int, stdin.readline().split()))
 L.sort()
 
 for i in combinations_with_replacement(L, N):
