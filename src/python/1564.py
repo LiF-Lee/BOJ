70fa8715e379b55d57e6f2d093d03d1db13ad395
Lee SJ <ei@genshin.ai>
2022-05-23 10:54:28 +0900

    no message

--- src/python/1564.py
+++ src/python/1564.py
@@ -1,4 +1,10 @@
 from sys import stdin, stdout
 from math import factorial
 a = int(stdin.readline())
-print(factorial(a))
+sv = 1
+for i in range(2, a + 1):
+    sv *= i
+    while sv % 10 == 0:
+        sv //= 10
+    sv %= 1_000_000_000_000
+print(str(sv)[-5:])
