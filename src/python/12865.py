57ecd6c35821ef2d0bf40d3d1b48c47912d87e19
Lee SJ <ei@genshin.ai>
2022-05-24 20:57:55 +0900

    no message

--- src/python/12865.py
+++ src/python/12865.py
@@ -1,25 +1,18 @@
 n, k = map(int, input().split())
 
 thing = [[0,0]]
-d = [[0]*(k+1) for _ in range(n+1)]
+d = [[0] * (k + 1) for _ in range(n + 1)]
 
 for i in range(n):
     thing.append(list(map(int, input().split())))
 
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        w = thing[i][0]
-        v = thing[i][1]
-
-        # print(i, j, w, v)
-
-        if j < w:
-            d[i][j] = d[i-1][j]
+for i in range(1, n + 1):
+    weight = thing[i][0]
+    value = thing[i][1]
+    for j in range(1, k + 1):
+        if j - weight >= 0:
+            d[i][j] = max(d[i - 1][j], d[i - 1][j - weight] + value)
         else:
-            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
-            print('+', i-1, j, w, v)
-            print('-', d[i-1][j], d[i-1][j-w]+v)
+            d[i][j] = d[i - 1][j]
 
-# print(d[n][k])
-print(d)
-print(thing)
+print(d[n][k])
