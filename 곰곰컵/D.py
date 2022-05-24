5b0743abfc800ad5cfc2b8f6c436cbf32f33f0a8
Lee SJ <ei@genshin.ai>
2022-05-24 20:58:03 +0900

    # Sliding_Window

--- 곰곰컵/D.py
+++ 곰곰컵/D.py
@@ -1,11 +1,17 @@
 from sys import stdin, stdout
 N = int(stdin.readline())
-today = 2
-for i in list(map(int, stdin.readline().split())):
-    today += i
-    if today % 7 == 6:
-        break
-if today % 7 == 6:
-    print('YES')
-else:
-    print('NO')
+works = map(int, stdin.readline().split())
+week = [0 for _ in range(7)]
+
+# 0: Sun ... 6: Sat
+week[1] = 1
+
+for i in works:
+    work_end = set()
+    for j in range(7):
+        if week[j] == 1 and week[(j + i) % 7] == 0:
+            work_end.add((j + i) % 7)
+    for k in work_end:
+        week[k] = 1
+
+print('YES' if week[5] == 1 else 'NO')
