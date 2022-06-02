e72ad178f4965c9976a32e0059e1e27dee5af28a
Lee SJ <ei@genshin.ai>
2022-06-02 23:46:30 +0900

    # KMP

---
+++ src/python/16916.py
@@ -0,0 +1,33 @@
+from sys import stdin, stdout
+T = stdin.readline().rstrip()
+P = stdin.readline().rstrip()
+
+def KMP_Search(txt, pat):
+    M, N = len(pat), len(txt)
+    lps = [0] * M
+    k, l, i, j = 1, 0, 0, 0
+    while k < M:
+        if pat[k] == pat[l]:
+            l += 1
+            lps[k] = l
+            k += 1
+        else:
+            if l != 0:
+                l = lps[l - 1]
+            else:
+                lps[k] = 0
+                k += 1
+    while i < N:
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+        elif pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i += 1
+        if j == M:
+            return 1
+    return 0
+KMP_Result = KMP_Search(T, P)
+print(KMP_Result)
