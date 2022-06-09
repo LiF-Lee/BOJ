453d940721c41ef26423ce82c4007f6dc986c5fb
DESKTOP-4S6M3IN\reeli <re.elif@icloud.com>
2022-06-09 22:40:38 +0900

    # 못끝냄

---
+++ src/python/11440.py
@@ -0,0 +1,14 @@
+from sys import stdin, stdout
+n = int(stdin.readline())
+
+DP = [0 for _ in range(n + 2)]
+DP[1] = 1
+DP2 = [0 for _ in range(n + 2)]
+DP2[1] = 1
+
+for i in range(2, n + 1):
+    DP[i] = DP[i - 1] + DP[i - 2]
+    DP2[i] = (DP[i] ** 2 % 1000000) + DP2[i - 1]
+
+print(DP)
+print(DP2)
