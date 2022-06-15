f61bda5671f6bf95eb6eba5616c4981caf3e3c32
DESKTOP-4S6M3IN\reeli <re.elif@icloud.com>
2022-06-15 20:38:55 +0900

    fix:  index error fix

--- src/python/9465.py
+++ src/python/9465.py
@@ -8,9 +8,10 @@ for _ in range(T):
     L[1] = list(map(int, stdin.readline().split()))
     DP[0][0] = L[0][0]
     DP[1][0] = L[1][0]
-    DP[0][1] = DP[1][0] + L[0][1]
-    DP[1][1] = DP[0][0] + L[1][1]
-    for i in range(2, n):
-        DP[0][i] = max(DP[1][i - 1], DP[1][i - 2]) + L[0][i]
-        DP[1][i] = max(DP[0][i - 1], DP[0][i - 2]) + L[1][i]
+    if n > 1:
+        DP[0][1] = DP[1][0] + L[0][1]
+        DP[1][1] = DP[0][0] + L[1][1]
+        for i in range(2, n):
+            DP[0][i] = max(DP[1][i - 1], DP[1][i - 2]) + L[0][i]
+            DP[1][i] = max(DP[0][i - 1], DP[0][i - 2]) + L[1][i]
     print(max(DP[0][n - 1], DP[1][n - 1]))
